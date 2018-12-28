619419d619a453ba3f417865953bea30ca1c8af4
Prasanna Brabourame <IS5442@integra.co.in>
2018-12-28 20:48:14 +0530

    two commit

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 self.onmessage = function (e) {
-    checkPrime = function (num) {
+    checkPrime = num => {
         var prime = 1;
         for (let b = 2; b < num; b++) {
             if (num % b == 0) {
@@ -12,34 +12,27 @@ self.onmessage = function (e) {
         else
             return 0;
     }
-    sieve = max => {
-        // Make array of length max and fill with true
-        const sieve = new Array(max).fill(true)
-      
-        // Iterate from 2 until square root of max
+    primeArry = max => {
+        const primeArry = new Array(max).fill(true)
         for (let i = 2; i < Math.sqrt(max); i++) {
-          // If the number is labelled a prime then we can start at i^2 and mark every multiple of i
-          // from there as NOT a prime
-          if (sieve[i]) {
-            for (let j = Math.pow(i, 2); j < max; j += i) {
-              sieve[j] = false
+            if (primeArry[i]) {
+                for (let j = Math.pow(i, 2); j < max; j += i) {
+                    primeArry[j] = false
+                }
             }
-          }
         }
-      
-        // Now we can reduce our sieve to only the Prime indexes that are true
-        return sieve.reduce((primes, isPrime, i) => {
-          if (isPrime && i > 1) {
-            primes.push(i)
-          }
-      
-          return primes
+        return primeArry.reduce((primes, isPrime, i) => {
+            if (isPrime && i > 1) {
+                primes.push(i)
+            }
+
+            return primes
         }, [])
-      }
+    }
     if (e.data.type === "sumOfPrimes") {
-            const max = sieve(Number(e.data.value));
-            const sumvalue = max.reduce((a,b)=>a+b);
-            self.postMessage({ value: sumvalue, type: e.data.type })
+        const max = primeArry(Number(e.data.value));
+        const sumvalue = max.reduce((a, b) => a + b);
+        self.postMessage({ value: sumvalue, type: e.data.type })
     } else if (e.data.type === "sumOfConsecutives") {
         var i = 2;
         var sum = 2;
