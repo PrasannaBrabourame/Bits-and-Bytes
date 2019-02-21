70002944c8003b66f3ec3ac47561539e05b0497b
Prasanna Brabourame <IS5442@integra.co.in>
2019-02-21 11:51:08 +0530

    few more functions2

--- worker.js
+++ worker.js
@@ -38,34 +38,94 @@ self.onmessage = function (e) {
         self.postMessage({ value: maxSum, type: e.data.type })
     } else if (e.data.type === "parenthesis") {
         const str = e.data.value;
-        const bracketsMap = new Map();
-        bracketsMap.set(']', '[');
-        bracketsMap.set('}', '{');
-        bracketsMap.set(')', '(');
+        if (str.length <= 1)
+            self.postMessage({ value: false, type: e.data.type })
 
-        const closingBrackets = [...bracketsMap.keys()];
-        const openingBrackets = [...bracketsMap.values()];
+        let matchingOpeningBracket, ch
+        let stack = []
 
-        var result = [];
-        var strLen = str.length;
-        for (i = 0; i < strLen; i++) {
-            ch = str[i];
-            if (openingBrackets.indexOf(ch) > -1) {
-                result.push(ch);
-            } else if (closingBrackets.indexOf(ch) > -1) {
-                var expectedBracket = bracketsMap.get(ch);
-                if (result.length === 0 || (result.pop() !== expectedBracket)) {
-                    self.postMessage({ value: "Invalid", type: e.data.type })
+        let openingBrackets = ['[', '{', '(']
+        let closingBrackets = [']', '}', ')']
+
+        for (let i = 0; i < str.length; i++) {
+            ch = str[i]
+
+            if (closingBrackets.indexOf(ch) > -1) {
+                matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)]
+                if (stack.length == 0 || (stack.pop() != matchingOpeningBracket)) {
+                    self.postMessage({ value: false, type: e.data.type })
                 }
             } else {
-                continue;
+                stack.push(ch)
             }
         }
-        self.postMessage({ value: (result === 0), type: e.data.type })
+        self.postMessage({ value: (stack.length == 0), type: e.data.type })
 
-    } else if(e.data.type === "reverseString"){
+    } else if (e.data.type === "reverseString") {
         const str = e.data.value;
         self.postMessage({ value: str.split('').reverse().join(''), type: e.data.type })
-    }
+    } else if (e.data.type === "perfectSquares") {
+        const str = e.data.value[0];
+        let result = [];
+        for (let i = e.data.value[0]; i <= e.data.value[1]; i++) {
+            if (i === 0) {
+                result.push(i)
+            }
+            else if (i % Math.sqrt(i) === 0) {
+                result.push(i)
+            }
+        }
+        self.postMessage({ value: result.toString(), type: e.data.type })
+    } else if (e.data.type === "camelCase") {
+        const text = e.data.value;
 
+        if (text === null || text === undefined) {
+            self.postMessage({ value: text, type: e.data.type })
+        }
+        var result = "";
+        var upper = 0;
+        var numeric = undefined;
+        var string = String(text);
+        for (var i = 0; i < string.length; i++) {
+            var ch = string[i];
+            var chLower = ch.toLowerCase();
+            if (ch !== chLower) {
+                var prev = result[result.length - 1];
+                if (upper > 1 && result.length > 1) {
+                    result = result.slice(0, result.length - 2) + prev;
+                }
+                if (result.length && prev !== "-" &&
+                    prev !== " " && prev !== "\t" && prev !== "\r" && prev !== "\n"
+                ) {
+                    result += "-";
+                }
+                result += chLower;
+                upper++;
+                numeric = false;
+            } else if (
+                ch === "0" || ch === "1" || ch === "2" || ch === "3" ||
+                ch === "4" || ch === "5" || ch === "6" || ch === "7" ||
+                ch === "8" || ch === "9"
+            ) {
+                if (numeric === false && result[result.length - 1] !== "-") {
+                    result += "-";
+                }
+                result += ch;
+                upper = 0;
+                numeric = true;
+            } else if (numeric && result[result.length - 1] !== "-") {
+                result += "-" + ch;
+                upper = 0;
+                numeric = false;
+            } else {
+                result += ch;
+                upper = 0;
+                numeric = false;
+            }
+        }
+        if (upper > 1 && result.length > 1) {
+            result = result.slice(0, result.length - 2) + result[result.length - 1];
+        }
+        self.postMessage({ value: result, type: e.data.type })
+    }
 };
